32f1c6b19ea46d7d66f0514923487316d6f5c6a7
Sam Hollow <zeac114@live.rhul.ac.uk>
2020-02-01 14:33:35 +0000

    Update ping method to use Response object

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,12 @@
 """Web server flask app."""
-from flask import Flask
+from flask import Flask, Response
 app = Flask(__name__)
 
 
 @app.route("/ping", methods=["GET"])
 def ping():
     """Ping the server."""
-    return "ping"
+    return Response("pong", status=200)
 
 
 if __name__ == "__main__":
