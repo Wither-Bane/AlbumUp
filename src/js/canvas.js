08d56afa4bc27063799961d51fee74620f06a248
Aidan Smithers <porrige51122>
2020-02-01 14:03:33 +0000

    Visualising Data on canvas

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -5,11 +5,20 @@ const ctx = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-let dataLength = Data.data.length;
-for (let i = 0; i < dataLength; i++) {
-  let d = Data.data[i];
-
-  console.log(d.length);
-}
 ctx.fillStyle = 0xFFFFFF;
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+let max = 0;
+for (let i = 0; i < Data.length; i++) {
+  if (Math.abs(Math.log(Data[i])) > max) {
+    max = Math.abs(Math.log(Data[i]));
+  }
+}
+
+ctx.strokeStyle = "#FFFFFF";
+ctx.lineWidth = 5;
+ctx.moveTo(0, canvas.height/2);
+for (let j = 0; j < Data.length; j++) {
+  ctx.lineTo((j + 1) * (canvas.width/Data.length), canvas.height/2 - (Math.log(Data[j]) * ((canvas.height/2)/max)));
+}
+ctx.stroke()
