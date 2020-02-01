7564d4fc2e355075b1eb6879241ab2504f3e34f6
Sam Hollow <zeac114@live.rhul.ac.uk>
2020-02-01 13:21:21 +0000

    Remove unused imports

--- proto/gen_spectrum.py
+++ proto/gen_spectrum.py
@@ -1,6 +1,5 @@
 """Generate json spectrum data."""
 from spectrum import arma_estimate, arma2psd, marple_data
-from pylab import plot, axis, xlabel, ylabel, grid, log10
 import json
 with open('dummy.json', 'w') as f:
     ar, ma, rho = arma_estimate(marple_data, 15, 15, 30)
