1e06606c37705a638a8c6a1c4a20cfb1608c3133
Sam Hollow <zeac114@live.rhul.ac.uk>
2020-02-01 18:38:10 +0000

    Update audio_to_pds to account for audio channels and update dummy.json

--- backend/frequency.py
+++ backend/frequency.py
@@ -1,5 +1,6 @@
 """Convert audio data into PSD."""
 import soundfile
+import numpy as np
 from spectrum import Periodogram
 
 
@@ -16,8 +17,10 @@ def audio_to_psd(file, hz=1.0):
     sig, samplerate = soundfile.read(file)
     period = int(samplerate / hz)
     psds = []
+    if len(sig.shape) > 1:  # If not mono, make mono
+        sig = sig.T[0]
     for i in range(period, sig.shape[0], period):
-        p = Periodogram(sig.T[0][(i - period):i], sampling=samplerate)
+        p = Periodogram(sig[(i - period):i], sampling=samplerate)
         p.run()
         psds.append(list(p.psd))
     return psds
