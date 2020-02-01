f7316b682cf0087bf4e5ee12ad860466144d5161
Sam Hollow <zeac114@live.rhul.ac.uk>
2020-02-01 18:02:27 +0000

    Add audio_to_pds function and update dummy.json

---
+++ backend/frequency.py
@@ -0,0 +1,23 @@
+"""Convert audio data into PSD."""
+import soundfile
+from spectrum import Periodogram
+
+
+def audio_to_psd(file, hz=1.0):
+    """Convert audio data into PSD.
+
+    Args:
+        file (str): Path to audio file.
+        hz (float): Frequency that the audio is split up.
+
+    Returns:
+        list: List of PSDs at sequential periods in the file
+    """
+    sig, samplerate = soundfile.read(file)
+    period = int(samplerate / hz)
+    psds = []
+    for i in range(period, sig.shape[0], period):
+        p = Periodogram(sig.T[0][(i - period):i], sampling=samplerate)
+        p.run()
+        psds.append(list(p.psd))
+    return psds
