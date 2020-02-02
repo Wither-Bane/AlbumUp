ba3d46c9de1d2c361cef5054cf9577bf97f8e02f
Aidan Smithers <porrige51122>
2020-02-02 02:46:23 +0000

    Pretty colors for tracks

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,8 +1,18 @@
 import Data from './data.json';
+import { Midi } from '@tonejs/midi';
 
+const colors = [
+  '#1BE7FF',
+  '#6EEB83',
+  '#E4FF1A',
+  '#E8AA14',
+  '#FF5714'
+]
+
+// const Data = Midi.fromUrl('./test1.mid');
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
-canvas.width = innerWidth
+canvas.width = innerHeight
 canvas.height = innerHeight
 
 ctx.fillStyle = 0xFFFFFF;
@@ -16,9 +26,10 @@ let maxNote = 90;
 let dif = canvas.width / (maxNote - minNote)
 for (let i = 0; i < dataLength; i++) {
   let d = Data.tracks[i].notes;
+  console.log(Data.tracks[i]);
+  ctx.fillStyle = colors[i % colors.length];
   for (let j = 0; j < d.length; j++) {
     let note = d[j];
-    ctx.fillStyle = "#FFFFFF";
     ctx.fillRect((note.midi-minNote) * dif, note.time * timeScale, dif, note.duration * timeScale);
   }
 }
